407d81cf4705710dcd990921533b1e5e8bac3615
Pedro Vieira <pedrovs3@hotmail.com>
2022-10-25 12:50:59 -0300

    Tasks stylized

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,30 +2,44 @@ import React, { Component } from 'react';
 import './Main.css';
 
 // Form
-import { FaPlus } from 'react-icons/fa';
+// Tarefas
+import { FaEdit, FaPlus, FaWindowClose } from 'react-icons/fa';
 
 export default class Main extends Component {
   state = {
-    novaTarefa: '',
+    newTask: '',
+    tasks: ['Fazer Café', 'Beber Agua', 'Estudar'],
   };
 
   handleChange = (event) => {
     this.setState({
-      novaTarefa: event.target.value,
+      newTask: event.target.value,
     });
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { newTask, tasks } = this.state;
 
     return (
       <div className="Main">
         <h1>Lista de tarefas</h1>
 
         <form action="#" className="Form">
-          <input onChange={this.handleChange} type="text" value={novaTarefa} />
+          <input onChange={this.handleChange} type="text" value={newTask} />
           <button type="submit">{<FaPlus />}</button>
         </form>
+
+        <ul className="Tasks">
+          {tasks.map((task, number) => (
+            <li className="Task" key={number}>
+              {task}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
