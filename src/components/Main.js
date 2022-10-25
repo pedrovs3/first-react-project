605185a97f0aadc9810d8cd0cfa7312546123f6a
Pedro Vieira <pedrovs3@hotmail.com>
2022-10-25 13:10:15 -0300

    Added insert tasks

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,28 @@ import { FaEdit, FaPlus, FaWindowClose } from 'react-icons/fa';
 export default class Main extends Component {
   state = {
     newTask: '',
-    tasks: ['Fazer Café', 'Beber Agua', 'Estudar'],
+    tasks: [],
   };
 
   handleChange = (event) => {
     this.setState({
       newTask: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { tasks } = this.state;
+    let { newTask } = this.state;
+    newTask = newTask.trim();
+
+    if (newTask === '') return;
+    if (tasks.includes(newTask)) return;
+
+    const newTasks = [...tasks];
+
+    this.setState({
+      tasks: [...newTasks, newTask],
     });
   };
 
@@ -24,7 +40,7 @@ export default class Main extends Component {
       <div className="Main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#" className="Form">
+        <form action="#" className="Form" onSubmit={this.handleSubmit}>
           <input onChange={this.handleChange} type="text" value={newTask} />
           <button type="submit">{<FaPlus />}</button>
         </form>
@@ -33,10 +49,10 @@ export default class Main extends Component {
           {tasks.map((task, number) => (
             <li className="Task" key={number}>
               {task}
-              <div>
+              <span>
                 <FaEdit className="edit" />
                 <FaWindowClose className="delete" />
-              </div>
+              </span>
             </li>
           ))}
         </ul>
