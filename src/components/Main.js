ac5aa455c4174a13a4929bbeb55c981d043cb7ef
Pedro Vieira <pedrovs3@hotmail.com>
2022-10-25 13:59:54 -0300

    Edit tasks enabled

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,6 +9,7 @@ export default class Main extends Component {
   state = {
     newTask: '',
     tasks: [],
+    index: -1,
   };
 
   handleChange = (event) => {
@@ -19,7 +20,7 @@ export default class Main extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const { tasks } = this.state;
+    const { tasks, index } = this.state;
     let { newTask } = this.state;
     newTask = newTask.trim();
 
@@ -28,13 +29,27 @@ export default class Main extends Component {
 
     const newTasks = [...tasks];
 
-    this.setState({
-      tasks: [...newTasks, newTask],
-    });
+    if (index === -1) {
+      this.setState({
+        tasks: [...newTasks, newTask],
+        newTask: '',
+      });
+    } else {
+      newTasks[index] = newTask;
+      this.setState({
+        tasks: [...newTasks],
+        index: -1,
+      });
+    }
   };
 
   handleEdit = (event, index) => {
-    console.log('edit', index);
+    const { tasks } = this.state;
+
+    this.setState({
+      index,
+      newTask: tasks[index],
+    });
   };
 
   handleDelete = (event, index) => {
